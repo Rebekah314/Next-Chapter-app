542415317208cb641851aea9e02ed62fd996e3d7
Rebekah Garris <rebekahlgarris@gmail.com>
2024-01-17 11:11:55 -0600

    added navigation links to homepage. also added line in application properties to change rest base-path

--- next-chapter/src/main/java/org/launchcode/nextchapter/controllers/HomeController.java
+++ next-chapter/src/main/java/org/launchcode/nextchapter/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package org.launchcode.nextchapter.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /* Created by Rebekah Garris */
@@ -10,7 +11,9 @@ public class HomeController {
 
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("title", "Welcome to your Next Chapter!");
         return "index";
     }
 }
