7985790daf82efda5459f2c0562345e5424b0f80
Rebekah Garris <rebekahlgarris@gmail.com>
2024-01-19 15:32:23 -0600

    added password fields for user and club

--- next-chapter/src/main/java/org/launchcode/nextchapter/models/User.java
+++ next-chapter/src/main/java/org/launchcode/nextchapter/models/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,13 +16,19 @@ public class User extends AbstractEntity{
 
     private String username;
 
+    @NotNull
+    private String pwHash;
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
 //    @ManyToMany(mappedBy = "members")
 //    private final List<Club> clubs = new ArrayList<>();
     //need to set up DTO, chapter 18.5
 
-    public User(String username, String displayName) {
+    public User(String username, String displayName, String password) {
         this.username = username;
         this.setDisplayName(displayName);
+        this.pwHash = encoder.encode(password);
     }
 
     public User() {}
@@ -31,6 +39,14 @@ public class User extends AbstractEntity{
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPwHash() {
+        return pwHash;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
     }
 
 //    public List<Club> getClubs() {
