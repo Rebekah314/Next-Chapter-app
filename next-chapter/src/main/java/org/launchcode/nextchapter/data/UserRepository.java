dc2138313d3cb7af8a4210599de9d2e43a248d1f
Rebekah Garris <rebekahlgarris@gmail.com>
2024-01-22 09:02:07 -0600

    updated app with authentication, updated homepage to direct to login and registration pages

--- next-chapter/src/main/java/org/launchcode/nextchapter/data/UserRepository.java
+++ next-chapter/src/main/java/org/launchcode/nextchapter/data/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+    User findByUsername(String username); //method signature creates SQL query
+
 }
