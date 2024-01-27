19b196f6d18f214016729018f4bc02990cf6aa5c
Molly Heininger <heininger.molly@gmail.com>
2024-01-27 15:40:42 -0600

    search.html now shows list of book titles

--- next-chapter/src/main/java/org/launchcode/nextchapter/data/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package org.launchcode.nextchapter.data;
-
-import org.launchcode.nextchapter.models.Blog;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepository extends CrudRepository<Blog, Integer> {
-}
