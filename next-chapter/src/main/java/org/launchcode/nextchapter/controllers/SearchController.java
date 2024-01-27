371f0bc6c6e78706535ecf6d1b00916326110e4f
Molly Heininger <heininger.molly@gmail.com>
2024-01-27 14:59:08 -0600

    added specific classes and added data cap to controller

--- next-chapter/src/main/java/org/launchcode/nextchapter/controllers/SearchController.java
+++ next-chapter/src/main/java/org/launchcode/nextchapter/controllers/SearchController.java
@@ -1,7 +1,11 @@
 package org.launchcode.nextchapter.controllers;
 
+import org.launchcode.nextchapter.models.SearchResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -15,19 +19,24 @@ public class SearchController {
     //passing in this argument below allows the method to build a Web Client (MH)
     public SearchController(WebClient.Builder webClientBuilder) {
         //below builds a Web Client which is able to make a call to the included URL (MH)
-        this.webClient = webClientBuilder
-                .baseUrl("https://openlibrary.org/search.json").build();
+        this.webClient = webClientBuilder.exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configurer
+                                .defaultCodecs()
+                                .maxInMemorySize(16 * 1024 * 1024))
+                .build()).baseUrl("https://openlibrary.org/search.json").build();
     }
 
-    public String getSearchResults(@RequestParam String query) {
+    @GetMapping("/search")
+    public String getSearchResults(@RequestParam String query, Model model) {
         //this builds a single GET request
         //fyi: Mono is a class object, as almost a placeholder for a future singular object(???)
-        Mono<String> foo = this.webClient.get()
+        Mono<SearchResult> resultsMono = this.webClient.get()
                 .uri("?q={query}", query)
                 //the uri above comes directly from the search URI of the external API (MH)
-                .retrieve().bodyToMono(String.class);
-        String bookTitle = foo.block();
-//Molly: using String is all just a placeholder until we create classes for the search results (which are arrays)
+                .retrieve().bodyToMono(SearchResult.class);
+        SearchResult result = resultsMono.block();
+        model.addAttribute("searchResult", result);
+
         return "search";
     }
 }
