f2881f677923389931a237a97666c23751f5a516
Rebekah Garris <rebekahlgarris@gmail.com>
2024-02-14 19:45:34 -0600

    updating club model to match main

--- next-chapter/src/main/java/org/launchcode/nextchapter/models/Club.java
+++ next-chapter/src/main/java/org/launchcode/nextchapter/models/Club.java
@@ -10,18 +10,26 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
-import static javax.swing.text.html.HTML.Tag.HEAD;
-
 @Entity
 public class Club extends AbstractEntity {
 
-   @ManyToMany
+    @ManyToMany
     private List<Member> members = new ArrayList<>();
     //need to set up DTO, chapter 18.5
 
     private String activeBook;
 
     private String adminPwHash;
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     private int adminId;
 
@@ -31,7 +39,7 @@ public class Club extends AbstractEntity {
     private final List<Blog> blogPosts = new ArrayList<>();
 
 
-    private String coverId = "99";
+    private String coverId = null;
 
     public String getCoverId() {
         return coverId;
@@ -42,16 +50,12 @@ public class Club extends AbstractEntity {
     }
 
 
-    public Club(String displayName, String activeBook, String password, int adminId) {
+    public Club(String displayName, String description, String activeBook, String password, int adminId) {
         this.setDisplayName(displayName);
+        this.description = description;
         this.activeBook = activeBook;
-<<<<<<< HEAD
-        CharSequence adminPw = null;
-        this.adminPwHash = encoder.encode(adminPw);
-
         this.adminPwHash = encoder.encode(password);
         this.adminId = adminId;
->>>>>>> origin/main
     }
 
     public Club() {}
@@ -62,7 +66,7 @@ public class Club extends AbstractEntity {
 
 
     public void setMembers(List<Member> members) {
-       this.members = members;
+        this.members = members;
     }
 
     public String getActiveBook() {
@@ -93,15 +97,9 @@ public class Club extends AbstractEntity {
         return encoder.matches(password, adminPwHash);
     }
 
-<<<<<<< HEAD
-    public void setAdminPwHash(String adminPwHash) {
-        this.adminPwHash = adminPwHash;
-    }
-=======
     public List<Blog> getBlogPosts() {
         return blogPosts;
     }
 
 
->>>>>>> origin/main
 }
